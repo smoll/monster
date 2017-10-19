61d7ab0cd285fc399b9f681b62d5f6089f5ace0f
Shujon Mollah <mollah@gmail.com>
2017-10-19 01:05:00 -0400

    Clean up bootstrap()

--- src/graphql/OfflineDatabase.js
+++ src/graphql/OfflineDatabase.js
@@ -18,21 +18,31 @@ class OfflineDatabase {
   }
 
   async bootstrap() {
+    // await this.run(`DROP TABLE IF EXISTS content;`)
+
     await this.run(`
       CREATE TABLE IF NOT EXISTS content (
         id INTEGER PRIMARY KEY,
-        title TEXT NOT NULL,
+        title TEXT UNIQUE NOT NULL,
         service TEXT NOT NULL,
         imdb REAL NOT NULL
       );
     `)
 
-    await this.run(`
-      INSERT INTO content VALUES (null, 'Breaking Bad', 'Netflix', 9.5);
-      INSERT INTO content VALUES (null, 'Cosmos: A Spacetime Odyssey', 'Netflix', 9.3);
-      INSERT INTO content VALUES (null, 'Sherlock', 'Netflix', 9.2);
-      INSERT INTO content VALUES (null, 'Transparent', 'Amazon', 7.9);
-    `)
+    const inserts = [
+      `INSERT INTO content VALUES (null, 'Breaking Bad', 'Netflix', 9.5);`,
+      `INSERT INTO content VALUES (null, 'Cosmos: A Spacetime Odyssey', 'Netflix', 9.3);`,
+      `INSERT INTO content VALUES (null, 'Sherlock', 'Netflix', 9.2);`,
+      `INSERT INTO content VALUES (null, 'Transparent', 'Amazon', 7.9);`,
+    ]
+
+    inserts.map(async q => {
+      try {
+        await this.run(q)
+      } catch(e) {
+        console.log(`${e} - ${q} - SKIPPED!!!`)
+      }
+    })
   }
 }
 
